313199a76225c8e1dd929b55731fe6730252e69d
Arunkumar Srisailapathi <sail.arunkumar@gmail.com>
2017-11-20 23:52:37 +0530

    more svg

--- src/app/drawing-board/drawing-board.component.ts
+++ src/app/drawing-board/drawing-board.component.ts
@@ -35,7 +35,40 @@ export class DrawingBoardComponent implements AfterViewInit {
     }
 
     private handleCallback(eventType: string, config: any, identifier: string): void {
-
+        if (eventType === 'click') {
+            switch (config.shape) {
+                case 'square':
+                    const c: any = {
+                        x1: 50,
+                        y1: 50,
+                        width: 100,
+                        height: 100,
+                        options: {
+                            stroke: '#000'
+                        },
+                        name: 'sq',
+                        type: 'element',
+                        shape: 'square'
+                    };
+                    this.drawShape(c);
+                    break;
+                case 'arrow':
+                    const a: any = {
+                        x1: 50,
+                        y1: 50,
+                        x2: 100,
+                        y2: 50,
+                        options: {
+                            stroke: '#000'
+                        },
+                        name: 'ar',
+                        type: 'element',
+                        shape: 'arrow'
+                    };
+                    this.drawShape(a);
+                    break;
+            }
+        }
     }
 
     private checkWhich(event: MouseEvent): Function {
@@ -85,9 +118,9 @@ export class DrawingBoardComponent implements AfterViewInit {
             },
             'arrow': {
                 x1: 50,
-                y1: 20,
+                y1: 10,
                 x2: 70,
-                y2: 20,
+                y2: 10,
                 direction: 'right',
                 name: 'ar-1',
                 type: 'indicator',
@@ -103,48 +136,79 @@ export class DrawingBoardComponent implements AfterViewInit {
     }
 
     private square(x1: number, y1: number, width: number, height: number, config?: any): void {
-        this.context.fillStyle = config.fill || null;
-        this.context.strokeStyle = config.stroke || null;
-        // this.context.lineWidth = 10;
-        if (config.fill) {
-            this.context.fillRect(x1, y1, width, height);
-        } else if (config.stroke) {
-            this.context.strokeRect(x1, y1, width, height);
-        }
+        let g = this.svg
+            .append('g')
+            .classed('dragging', true);
+
+        g   .append('rect')
+            .attr('name', config.name)
+            .attr('class', 'rectangle')
+            .attr('width', width)
+            .attr('height', height)
+            .attr('fill', '#fff')
+            .attr('stroke', config.options.stroke)
+            .on('click', () => {
+                if (config.type === 'indicator') {
+                    this.handleCallback('click', config, config.name);
+                }
+            })
+            .on('dblclick', function () {
+                event.preventDefault();
+                g   .append('text')
+                    .attr('x', x1 + (x1 / 2))
+                    .attr('y', height / 2)
+                    .attr('contentEditable', true)
+                    .attr('dy', '.35em')
+                    .text('Text here')
+                    .on('keyup', function() { this.text = d3.select(this).text(); });
+            })
+            .call(d3 .drag()
+                .on('start', function() {
+                    console.log('start');
+                })
+                .on('drag', function(d) {
+                    console.log(d3.event.x);
+                    this.setAttribute('x', d3.event.x);
+                    g.select('text').attr('x', d3.event.x);
+                })
+            );
     }
 
-    private arrow(x1: number, y1: number, x2: number, y2: number, direction?: string, options?: any): void {
-        // if (!direction) direction = 'right';
+    private arrow(x1: number, y1: number, x2: number, y2: number, direction?: string, config?: any): void {
+        this.svg.append('line')
+             .attr('x1', x1)
+             .attr('y1', y1)
+             .attr('x2', x2)
+             .attr('y2', y2)
+             .attr('stroke', config.options.stroke)
+             .attr('stroke-width', 2)
+             .on('click', () => {
+                if (config.type === 'indicator') {
+                    this.handleCallback('click', config, config.name);
+                }
+            })
+            .call(d3 .drag()
+                .on('start', function() {
+                    console.log('start');
+                })
+                .on('drag', function(d) {
+                    console.log(d3.event.x);
+                    const current = d3.event.x;
 
-        const ctx: CanvasRenderingContext2D = this.context;
-
-        const lineLength: number = x2 - x1;
-        const arrowRadius: number = Math.ceil(lineLength / 10);
-
-        ctx.beginPath();
-        ctx.strokeStyle = options.stroke || '#000';
-        ctx.fillStyle = options.fill || '#000';
-
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-
-        // ctx.lineTo(x2 - arrowRadius, y2 - arrowRadius);
-        // ctx.arcTo(x2, y2, x2 - arrowRadius, x2 + arrowRadius, arrowRadius + 10);
-
-        // ctx.lineTo(x2, y2);
-
-        ctx.stroke();
-        ctx.fill();
+                    this.setAttribute('x1', current);
+                    this.setAttribute('x2', current + (x2 - x1));
+                })
+            );
     }
 
     drawShape(config: any): void {
         let flag = true;
         switch (config.shape) {
             case 'square':
-                this.square(config.x1, config.y1, config.width, config.height, config.options);
+                this.square(config.x1, config.y1, config.width, config.height, config);
                 break;
             case 'arrow':
-                this.arrow(config.x1, config.y1, config.x2, config.y2, config.direction, config.options);
+                this.arrow(config.x1, config.y1, config.x2, config.y2, config.direction, config);
                 break;
             default:
                 flag = false;
