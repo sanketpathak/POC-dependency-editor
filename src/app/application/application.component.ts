02c4ff06d483f37d058dd16f12df7a4278bb7f34
sanketpathak <sanketpathak95@gmail.com>
2017-12-27 03:20:08 +0530

    UI changes related to your application

--- src/app/application/application.component.ts
+++ src/app/application/application.component.ts
@@ -1,3 +1,4 @@
+<<<<<<< 0a4685ef8d27d3aeb80cc397058331117072944b
 import {
   Component,
   OnInit,
@@ -7,6 +8,9 @@ import {
   EventEmitter,
   ViewEncapsulation
 } from '@angular/core';
+=======
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter, ViewEncapsulation, SimpleChanges } from '@angular/core';
+>>>>>>> UI changes related to your application
 import { AccordionModule } from 'ngx-bootstrap';
 import { HttpModule } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
@@ -22,6 +26,7 @@ import * as c3 from 'c3';
   templateUrl: './application.component.html'
 })
 export class ApplicationComponent implements OnInit, OnChanges {
+<<<<<<< 0a4685ef8d27d3aeb80cc397058331117072944b
   @Input('component') component;
   @Output() onCloseEmitter = new EventEmitter();
   @Input('dependencies') dependencies;
@@ -50,11 +55,59 @@ export class ApplicationComponent implements OnInit, OnChanges {
   public selectedComp = new Set();
   public compDep = new Set();
   public charts: any = {};
+=======
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error("Method not implemented.");
+  }
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+  @Input('component') component;
+  @Output() onCloseEmitter = new EventEmitter();
+
+  @Input('dependencies') dependencies;
+  @Input('selectedDependenciesObject') selectedDependenciesObject;
+
+  public donutData: any;
+  public showtd: boolean = false;
+  public symbol: string = 'fa fa-sort-desc';
+  public showOnScreen: boolean = true;
+  public l1: string;
+  public l2: string;
+  public l3: string;
+  public rel: string = "lgplv3+";
+  public r_l:number;
+  public moreInfoText: string = "";
+  public cve=0;
+  public dep: Array<any> = [];
+  public securityConflicts = ["aws-maven"];
+  public isSecurityIssueVar:string  = '';
+
+  
+  public compDep = new Set();
+ 
+  public charts: any = {};
+
+  public isSecurityIssue(): void {
+    console.log("I am running");
+    this.isSecurityIssueVar = '';            
+    if(this.dependencies)
+    this.dependencies['dependencies'].forEach((d) => {
+      if (this.securityConflicts.indexOf(d.name) !== -1){
+        this.isSecurityIssueVar = d.name;      
+        this.cve = 1;       
+      }
+    })
+  }
+  constructor(private applicationServices: ApplicationServices) { }
+
+>>>>>>> UI changes related to your application
   status: any = {
     isFirstOpen: true,
     isFirstDisabled: false
   };
 
+<<<<<<< 0a4685ef8d27d3aeb80cc397058331117072944b
   public isSecurityIssue(): void {
     this.isSecurityIssueVar = '';
     this.cve = 0;
@@ -63,6 +116,43 @@ export class ApplicationComponent implements OnInit, OnChanges {
         if (this.securityConflicts.indexOf(d.name) !== -1) {
           this.isSecurityIssueVar = d.name;
           this.cve += 1;
+=======
+public moreInfo() {
+this.moreInfoText = "some description of CVE";
+}
+
+public removeItem(dependency):void{
+  // if(dependency === this.dependencies){
+    if(this.dependencies){
+      this.dependencies.dependencies.delete(dependency);
+      this.dependencies.companionPackages.add(dependency);
+    }else {
+      this.compDep.add(dependency);
+      this.dependencies.dependencies.delete(dependency);
+    }
+//  }
+//  else{
+  // if(this.dependencies){
+  //   this.dependencies.dependencies.delete(dependency);
+  //   this.dependencies.companionPackages.add(dependency);
+  // }else {
+  //   this.compDep.add(dependency);
+  //   this.dependencies.dependencies.delete(dependency);
+  // }
+//  }
+}
+
+// public remove_dep_Item(dependencies):void{
+  
+// }
+
+processPackages(): void {
+    const packageInfo: Observable<any> = this.applicationServices.getPackages();
+    packageInfo.subscribe((data) => {
+        console.log(data);
+        if (data) {
+            this.dep = data['dependencies'];
+>>>>>>> UI changes related to your application
         }
       });
     }
