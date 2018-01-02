fa8739cf0d0be2ced3f6795d6cf4694142da05df
invinciblejai <mailjai.vardhan@gmail.com>
2018-01-02 14:46:05 +0530

    updates chat text
    Signed-off-by: invinciblejai <mailjai.vardhan@gmail.com>

--- src/app/data/chat-example-data.ts
+++ src/app/data/chat-example-data.ts
@@ -46,7 +46,7 @@ const initialMessages: Array<Message> = [
     sentAt: moment()
       .subtract(1, 'minutes')
       .toDate(),
-    text: `Hi, I detected that you are looking to secure your application. I think you might be interested by depXXX instead of depYYY which is having less security issues reported over the last 6months.`,
+    text: `For Springboot application with CRUD operations, we suggest redis than memcache.`,
     thread: tEcho
   }),
   new Message({
@@ -85,7 +85,7 @@ export class ChatExampleData {
   static setupBots(messagesService: MessagesService): void {
     // echo bot
     let response = '';
-    const q1 = ['tell', 'popular'];
+    const q1 = ['tell', 'popular', 'more'];
     const q2 = ['ok', 'do', 'change'];
     const quries = [q1, q2];
     messagesService
@@ -102,7 +102,7 @@ export class ChatExampleData {
             });
             if (matched){
               switch (q){
-                case q1: response = 'It is very active on github, the community is also very reactive - in general issues are responded in less than 12h';
+                case q1: response = `It has no CVEs associated. <br/> Has very active community with over than 26K stars on gitHub`;
                   break;
                 case q2: response = 'Done';
                   break;
