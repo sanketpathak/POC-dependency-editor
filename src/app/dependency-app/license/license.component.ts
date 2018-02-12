078bb49dc38ef5c2701471f06f0d8d71d4a89a87
Jyasveer Gotta <jyasveer@gmail.com>
2018-02-13 00:49:56 +0530

    more changes

---
+++ src/app/dependency-app/license/license.component.ts
@@ -0,0 +1,56 @@
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  Input,
+  Output,
+  EventEmitter,
+  ViewEncapsulation
+} from '@angular/core';
+import {
+  TagInputModule
+} from 'ngx-chips';
+import {
+  FormsModule
+} from '@angular/forms';
+import {
+  AccordionModule
+} from 'ngx-bootstrap';
+import { StackLicenseAnalysisModel } from '../model/data.model';
+
+@Component({
+  selector: 'app-license',
+  templateUrl: './license.component.html',
+  styleUrls: ['./license.component.less']
+})
+
+export class LicenseComponent implements OnInit, OnChanges {
+  @Input() licenseData: StackLicenseAnalysisModel;
+
+  public title = 'License';
+  public icon = 'fa fa-file-text-o';
+  public stackLicense: string;
+  public hasIssue: boolean | string = false;
+
+  constructor() {}
+
+  ngOnChanges() {
+    if (this.licenseData) {
+      if (this.licenseData.status.toLowerCase() === 'successful') {
+        this.hasIssue = false;
+        this.stackLicense = this.licenseData.f8a_stack_licenses[0];
+      } else if (this.licenseData.status.toLowerCase() === 'failure') {
+        this.hasIssue = 'na';
+        this.stackLicense = 'Unknown';
+      } else if (this.licenseData.status.toLowerCase() === 'conflict' || this.licenseData.status.toLowerCase() === 'unknown') {
+        this.hasIssue = true;
+        this.stackLicense = 'None';
+      }
+    } else {
+      this.stackLicense = null;
+    }
+  }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/dependency-app/security/security.component.ts
@@ -0,0 +1,55 @@
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  Input,
+  Output,
+  EventEmitter,
+  ViewEncapsulation
+} from '@angular/core';
+import {
+  TagInputModule
+} from 'ngx-chips';
+import {
+  FormsModule
+} from '@angular/forms';
+import {
+  AccordionModule
+} from 'ngx-bootstrap';
+import { CveResponseModel } from '../model/data.model';
+
+@Component({
+  selector: 'app-security',
+  templateUrl: './security.component.html',
+  styleUrls: ['./security.component.less']
+})
+
+export class SecurityComponent implements OnInit, OnChanges {
+  @Input() cveData: CveResponseModel;
+
+  public title = 'Security Alert';
+  public icon = 'fa fa-shield';
+  public noOfCves = 0;
+  public hasIssue = false;
+
+  constructor() {}
+
+  ngOnChanges() {
+    if (this.cveData) {
+      this.noOfCves = 0;
+      this.cveData.result.forEach(item => {
+        if (item.cve) {
+          this.noOfCves++;
+          this.hasIssue = true;
+        }
+      });
+    } else {
+      this.noOfCves = null;
+    }
+  }
+
+  ngOnInit() {
+
+  }
+
+}
