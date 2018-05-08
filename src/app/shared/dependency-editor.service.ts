a49a192fc515e3bb1a3d997a9bc2733939f87112
sanketpathak <sanketpathak95@gmail.com>
2018-05-08 15:20:08 +0530

    post stack analyses payload edited

--- src/app/shared/dependency-editor.service.ts
+++ src/app/shared/dependency-editor.service.ts
@@ -63,9 +63,8 @@ export class DependencyEditorService {
         const url = this.RECOMMENDER_API_BASE + 'api/v1/stack-analyses';
         return this.options.flatMap((option) => {
             // const payload = 'github_url=' + githubUrl;
-            const payload: any = {};
-            payload['github_url'] = githubUrl + '&source=osio';
-            payload['github_ref'] = githubRef;
+            let payload: any;
+            payload = 'github_url=' + githubUrl + '&source=osio' + '&github_ref=' + githubRef;
             option.headers.append('Content-Type', 'application/x-www-form-urlencoded');
             return this.http.post(url, payload, option)
                 .map(this.extractData)
